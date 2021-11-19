3d17317bfe2296986df276d2c1c612b1591b49fc
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2021-11-19 12:41:43 -0500

    Added admin pages

--- server/controller/admin.js
+++ server/controller/admin.js
@@ -1,10 +1,55 @@
 let express = require("express");
 
-module.exports.displayAdminHomePage = (req, res, next) => {
+module.exports.displayHomePage = (req, res, next) => {
     if(!req.user)
     {
         return res.redirect("/login");
     }
     res.render("../views/admin/home", { title: "Admin Home Page", 
+    username: req.user ? req.user.username: ''  });
+  };
+
+  module.exports.displayApprovedPage = (req, res, next) => {
+    if(!req.user)
+    {
+        return res.redirect("/login");
+    }
+    res.render("../views/admin/approved", { title: "Approved Page", 
+    username: req.user ? req.user.username: ''  });
+  };
+
+  module.exports.displayPendingPage = (req, res, next) => {
+    if(!req.user)
+    {
+        return res.redirect("/login");
+    }
+    res.render("../views/admin/pending", { title: "Pending Page", 
+    username: req.user ? req.user.username: ''  });
+  };
+
+  module.exports.displayRejectedPage = (req, res, next) => {
+    if(!req.user)
+    {
+        return res.redirect("/login");
+    }
+    res.render("../views/admin/rejected", { title: "Rejected Page", 
+    username: req.user ? req.user.username: ''  });
+  };
+
+  module.exports.displayTask1Page = (req, res, next) => {
+    if(!req.user)
+    {
+        return res.redirect("/login");
+    }
+    res.render("../views/admin/task1", { title: "Task 1 Page", 
+    username: req.user ? req.user.username: ''  });
+  };
+
+  module.exports.displayTask2Page = (req, res, next) => {
+    if(!req.user)
+    {
+        return res.redirect("/login");
+    }
+    res.render("../views/admin/task2", { title: "Task 2 Page", 
     username: req.user ? req.user.username: ''  });
   };
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,6 +2,17 @@ let express = require("express");
 let router = express.Router();
 let adminController = require("../controller/admin");
 
-router.get("/admin", adminController.displayAdminHomePage);
+router.get("/admin", adminController.displayHomePage);
+router.get("/admin/home", adminController.displayHomePage);
+
+router.get("/admin/approved", adminController.displayApprovedPage);
+
+router.get("/admin/pending", adminController.displayPendingPage);
+
+router.get("/admin/rejected", adminController.displayRejectedPage);
+
+router.get("/admin/task1", adminController.displayTask1Page);
+
+router.get("/admin/task2", adminController.displayTask2Page);
 
 module.exports = router;
